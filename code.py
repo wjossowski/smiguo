4b78f8afc60598d3efbeaa1b463867fdcceeb4ba
Wojciech Ossowski <w.j.ossowski@gmail.com>
2023-04-25 17:43:28 +0200

    Controller done

--- code.py
+++ code.py
@@ -2,44 +2,18 @@ import board
 from time import sleep
 import microcontroller
 import wifi
+from adafruit_httpserver.server import HTTPServer
+
 from display import MyDisplay
 from servo import MyServo
 from ssid_connect import pool
-
-from adafruit_httpserver.mime_type import MIMEType
-from adafruit_httpserver.request import HTTPRequest
-from adafruit_httpserver.response import HTTPResponse
-from adafruit_httpserver.server import HTTPServer
-from adafruit_httpserver.methods import HTTPMethod
+from controller import Controller
 
 servo = MyServo(pwm=board.GP0, left=board.GP15, right= board.GP14)
 display = MyDisplay(rs=board.GP9, en=board.GP8, d7=board.GP13, d6=board.GP12, d5=board.GP11, d4=board.GP10)
-
-def set_motor_pos(angle, source = "Manual"):
-    print("Setting angle to:", angle, "Source:", source)
-    servo.set_position(angle)
-    display.write(f"Angle: {servo.angle}", source)
-
 server = HTTPServer(pool)
-@server.route("/", method=HTTPMethod.GET)
-def r_root(request: HTTPRequest):
-    with HTTPResponse(request, content_type=MIMEType.TYPE_HTML) as response:
-        response.send_file("index.html")
-
-@server.route("/motor", method=HTTPMethod.POST)
-def r_move(request: HTTPRequest):
-    angle = int(request.query_params.get("a") or 90)
-    with HTTPResponse(request, content_type=MIMEType.TYPE_TXT) as response:
-        addr, _ = request.client_address
-        set_motor_pos(angle, addr)
-        response.send(f"Rotor position set to {angle}", angle)
-
-@server.route("/data", method=HTTPMethod.GET)
-def r_data(request:HTTPRequest):
-    with HTTPResponse(request, content_type=MIMEType.TYPE_JSON) as response:
-        response.send(f'{{ "angle": {servo.angle}, "text": "{display.msg}" }}')
-
-print("starting server..")
+controller = Controller(server, servo, display).combine_routes()
+controller.update_display()
 
 try:
     server.start(str(wifi.radio.ipv4_address))
@@ -52,13 +26,14 @@ except OSError:
 while True:
     try:
         if not servo.left.value:
-            servo.step_backward()
+            controller.manual_step_backward()
         elif not servo.right.value:
-            servo.step_forward()
+            controller.manual_step_forward()
         else:
             server.poll()
             sleep(0.5)
     except Exception as e:
         print(e)
+        sleep(1)
         continue
 
---
+++ controller.py
@@ -0,0 +1,52 @@
+from adafruit_httpserver.mime_type import MIMEType
+from adafruit_httpserver.request import HTTPRequest
+from adafruit_httpserver.response import HTTPResponse
+from adafruit_httpserver.methods import HTTPMethod
+from adafruit_httpserver.server import HTTPServer
+from display import MyDisplay
+from servo import MyServo
+
+class Controller:
+    server: HTTPServer
+    motor: MyServo
+    display: MyDisplay
+
+    def __init__(self, server: HTTPServer, servo: MyServo, display: MyDisplay ):
+        self.server = server
+        self.motor = servo
+        self.display = display
+
+    def combine_routes(self):
+        @self.server.route("/", method=HTTPMethod.GET)
+        def r_root(request: HTTPRequest):
+            with HTTPResponse(request, content_type=MIMEType.TYPE_HTML) as response:
+                response.send_file("index.html")
+
+        @self.server.route("/motor", method=HTTPMethod.POST)
+        def r_move(request: HTTPRequest):
+            angle = int(request.query_params.get("a") or 90)
+            with HTTPResponse(request, content_type=MIMEType.TYPE_TXT) as response:
+                self.motor.set_position(angle)
+                addr, _ = request.client_address
+                self.update_display(addr)
+                response.send(f"Rotor position set to {angle}", angle)
+
+        @self.server.route("/data", method=HTTPMethod.GET)
+        def r_data(request:HTTPRequest):
+            with HTTPResponse(request, content_type=MIMEType.TYPE_JSON) as response:
+                response.send(f'{{ "angle": {self.motor.angle}, "text": "{self.display.msg}" }}')
+        print("starting server..")
+        return self
+    
+    def manual_step_forward(self):
+        self.motor.step_forward()
+        self.update_display()
+
+    def manual_step_backward(self):
+        self.motor.step_backward()
+        self.update_display()
+
+    def update_display(self, source = "Manual"):
+        print("Setting angle to:", self.motor.angle, "Source:", source)
+        self.display.write(f"Angle: {self.motor.angle}", source)
+
